a89c415c2bd2e4951398286e1f3fa27e76dd277f
AndersonGcH <andersoncutipa113@gmail.com>
2026-05-21 18:57:59 -0500

    feat: DTO usuario agregado

--- src/main/java/com/vortexis/controllers/UsuarioController.java
+++ src/main/java/com/vortexis/controllers/UsuarioController.java
@@ -1,6 +1,8 @@
 package com.vortexis.controllers;
+import com.vortexis.dto.UsuarioRequestDTO;
 import com.vortexis.entities.Usuario;
 import com.vortexis.services.UsuarioService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -18,7 +20,18 @@ public class UsuarioController {
     }
 
     @PostMapping
-    public Usuario guardarUsuario(@RequestBody Usuario usuario) {
+    public Usuario guardarUsuario(
+            @RequestBody @Valid UsuarioRequestDTO dto
+    ) {
+
+        Usuario usuario = Usuario.builder()
+                .nombre(dto.getNombre())
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .rol(dto.getRol())
+                .activo(true)
+                .build();
+
         return usuarioService.guardarUsuario(usuario);
     }
 
@@ -30,8 +43,16 @@ public class UsuarioController {
     @PutMapping("/{id}")
     public Usuario actualizarUsuario(
             @PathVariable Long id,
-            @RequestBody Usuario usuario
-    ) {
+            @RequestBody @Valid UsuarioRequestDTO dto)
+    {
+        Usuario usuario = Usuario.builder()
+                .nombre(dto.getNombre())
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .rol(dto.getRol())
+                .activo(true)
+                .build();
+
         return usuarioService.actualizarUsuario(id, usuario);
     }
 
