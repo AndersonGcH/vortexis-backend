aa5298747b993e3c721337fcaf3a7638eb07e9a6
AndersonGcH <andersoncutipa113@gmail.com>
2026-05-21 02:48:03 -0500

    Creacion del proyecto

---
+++ src/test/java/com/vortexis/VortexisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vortexis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VortexisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
